c41f548c04149b9e1361d06ff601f09ce7d4f1f8
sby-a-izumi <a-izumi@shibuya.co.jp>
2025-06-04 15:17:09 +0900

    Implement basic binding logic in OthelloGame ViewModel

--- src/ViewModels/OthelloGameViewModel.cs
+++ src/ViewModels/OthelloGameViewModel.cs
@@ -1,7 +1,29 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using BlazorOthello.Models;
+
 namespace BlazorOthello.ViewModels
 {
-    public class OthelloGameViewModel
+    public class OthelloGameViewModel : INotifyPropertyChanged
     {
-        // ViewModel logic, binding properties, commands, etc.
+        private OthelloGame game;
+
+        public OthelloGameViewModel()
+        {
+            game = new OthelloGame();
+            game.InitializeBoard();
+        }
+
+        public Player GetPlayerAt(int x, int y)
+        {
+            return game.GetPlayerAt(x, y);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
